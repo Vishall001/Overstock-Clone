f110fd5c515b507d110830c79ab3d9684ad121c3
Vishall001 <vshmandloi@gmail.com>
2022-08-15 09:16:25 +0530

    Clone of Overstock website

---
+++ home.js
@@ -0,0 +1,91 @@
+document.getElementById("logo").addEventListener("click",mylogo)
+
+function mylogo(){
+    window.location.href="index.html"
+}
+
+// account page
+document.getElementById("acc").addEventListener("click",myacc)
+
+function myacc(){
+    window.location.href="signup.html"
+}
+
+// whislistpage
+document.getElementById("whislist").addEventListener("click",mywhis)
+
+function mywhis(){
+    window.location.href=""
+}
+
+//cart page
+document.getElementById("cart").addEventListener("click",mycart)
+function mycart(){
+    window.location.href=""
+}
+// payment page
+document.querySelector("#gotopayment").addEventListener("click",mypay)
+function mypay(){
+    window.location.href="payment.html"
+}
+
+
+//main body
+
+//first
+
+document.getElementById("first").addEventListener("click",myfirst)
+function myfirst(){
+    window.location.href="AllProducts.html"
+}
+
+
+
+//second
+document.getElementById("secbox").addEventListener("click",secbox)
+function secbox(){
+    window.location.href="BathroomVanityProd.html"
+}
+
+
+
+// third
+document.getElementById("third").addEventListener("click", mythird)
+function mythird(){
+    window.location.href="rugs.html"
+}
+
+
+// /fourth
+document.getElementById('fourth').addEventListener("click",myfourth)
+function myfourth(){
+    window.location.href="Kitchen.html"
+}
+
+
+//fifth
+document.getElementById("fifthbox").addEventListener("click",myfifth)
+function myfifth(){
+    window.location.href="rugs.html"
+}
+
+
+//sixth
+document.getElementById("sixth").addEventListener("click",mysixth)
+function mysixth(){
+    window.location.href="LRFurniture.html"
+}
+
+
+
+//seven
+document.getElementById("sevenbox").addEventListener("click",myseven)
+function myseven(){
+    window.location.href="AllProducts.html"
+}
+
+// btn 
+document.getElementById("btn").addEventListener("click",mybtn)
+function mybtn(){
+    window.location.href="AllProducts.html"
+}
